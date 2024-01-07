b7cfabf92a72e1427d6a06d47593346c12e065cd
Anteneh Bedilu <anteneh@antenehbedilu.com>
2024-01-07 21:50:07 +0000

    feat(backend/main.py): add metadata and docs urls

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,20 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+        docs_url='/api/docs', #set the URL for the API documentation
+        openapi_url='/api/openapi', #set the URL for the OpenAPI schema
+        redoc_url=None, #disable the ReDoc documentation
+        title='Exif', #set the title of the API
+        description='This is a simple **RESTful API** for modifying **Exif** *(exchangeable image file format)* metadata.', #set the description of the API
+        summary='View, Remove and Edit Exif metadata.', #set a summary for the API
+        version='0.0.1', #set the version of the API
+        contact={ #set the contact information for the API
+            'name': 'Exif',
+            'url': 'https://github.com/antenehbedilu/exif',
+            'email': 'hello@antenehbedilu.com'
+            },
+        license_info={ #set the license information for the API
+            'name': 'MIT License',
+            'url': 'https://raw.githubusercontent.com/antenehbedilu/exif/main/LICENSE'
+            }
+        )
