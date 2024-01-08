a4975e767dba05996806eaa2bc4877b708812135
Anteneh Bedilu <anteneh@antenehbedilu.com>
2024-01-08 10:11:45 +0000

    feat(backend/main.py): edit metadata and redoc url

--- backend/main.py
+++ backend/main.py
@@ -5,10 +5,10 @@ from routers import health
 app = FastAPI(
         docs_url='/api/docs', #set the URL for the API documentation
         openapi_url='/api/openapi', #set the URL for the OpenAPI schema
-        redoc_url=None, #disable the ReDoc documentation
+        redoc_url='/api/redoc', #disable the ReDoc documentation
         title='Exif', #set the title of the API
-        description='This is a simple **RESTful API** for modifying **Exif** *(exchangeable image file format)* metadata.', #set the description of the API
-        summary='View, Remove and Edit Exif metadata.', #set a summary for the API
+        description='This **RESTful API** is designed to modify **Exif** *(exchangeable image file format)* metadata.', #set the description of the API
+        summary='Exif easily view, edit, and remove metadata.', #set a summary for the API
         version='0.0.1', #set the version of the API
         contact={ #set the contact information for the API
             'name': 'Exif',
